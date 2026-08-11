7cc5b920bc8faa6cb0c27fb3c2ceb66b40649377
Gustavo <gucesar339@gmail.com>
2026-08-11 19:50:55 -0300

    ajusta variaveis

--- src/app/core/config/api.config.ts
+++ src/app/core/config/api.config.ts
@@ -1,1 +1,6 @@
-export const API_BASE_URL = 'http://localhost:8080/api/v1';
+const isProd = typeof window !== 'undefined' && window.location.hostname !== 'localhost';
+
+export const API_BASE_URL = isProd
+  ? 'https://souzas-finance-eudrdnh4c2hzgzgu.eastus2-01.azurewebsites.net/api/v1'
+  : 'http://localhost:8080/api/v1';
+
